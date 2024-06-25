74fb282cf754d3ac67ec7b91099628de16f69a5f
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-06-25 17:42:42 -0500

    frontend layout set for the whole application with redux and paypal in place too

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-function App() {
+const App = () => {
   return (
-  <h1 className="text-3xl font-bold underline">Hello World!</h1>
-);
-}
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
