0cbbd0d2e188bfea9a435de181aa900c4a24764c
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-06-25 15:26:29 -0500

    user update feature completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import {createUser, loginUser, logoutCurrentUser, getAllUsers} from '../controllers/userController.js';
+import {createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile} from '../controllers/userController.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 
-router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router.route('/')
+.post(createUser)
+.get(authenticate, authorizeAdmin, getAllUsers);
+
 router.post('/auth', loginUser);
 router.post('/logout', logoutCurrentUser);
+
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 
   
