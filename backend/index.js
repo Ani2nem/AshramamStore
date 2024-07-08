5823339dee2e1fb6e5a14de71c68927b95a1ddd5
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-08 16:17:07 -0500

    backend for order delivery completed, need to work on order frontend and redux as well as paypal payment integration

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import connectDB from "./config/db.js";
 import errorMiddleware from './middlewares/errorMiddleware.js';
+
 
 dotenv.config();
 const port = process.env.PORT || 5001;
@@ -24,6 +26,7 @@ app.use("/api/users", userRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/upload', uploadRoutes);
+app.use('/api/orders', orderRoutes);
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
