fab4156f6a62060a8bf76435855e68b5703adf10
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-11 18:55:06 -0500

    added cors, modified apiSlice.js, constants.js for production, added error handling in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
@@ -34,10 +35,34 @@ app.get('/api/config/paypal', (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // handle errors in middleware
 app.use(errorMiddleware);
+
+
+app.use(cors({
+    origin: 'https://anirudh-e-store-frontend.onrender.com',
+    credentials: true,
+}));
+
+
+// error handling
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Route not found" });
+  });
+
+  app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+  });
+
+  app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK' });
+  });
+
+
+
 
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
