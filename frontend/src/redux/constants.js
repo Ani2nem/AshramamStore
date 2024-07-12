c6585b9326dbc2b44348830efc4a99900ca90e4b
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-12 14:07:39 -0500

    BaseURL correted

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = ""; 
-export const USERS_URL = "https://anirudh-e-store.onrender.com/api/users";
-export const CATEGORY_URL = "https://anirudh-e-store.onrender.com/api/category";
-export const PRODUCT_URL = "https://anirudh-e-store.onrender.com/api/products";
-export const UPLOAD_URL = "https://anirudh-e-store.onrender.com/api/upload";
-export const ORDERS_URL = "https://anirudh-e-store.onrender.com/api/orders";
-export const PAYPAL_URL = "https://anirudh-e-store.onrender.com/api/config/paypal";
+export const BASE_URL = "https://anirudh-e-store.onrender.com"; 
+export const USERS_URL = "/api/users";
+export const CATEGORY_URL = "/api/category";
+export const PRODUCT_URL = "/api/products";
+export const UPLOAD_URL = "/api/upload";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
