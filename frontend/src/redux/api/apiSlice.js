2aca89b8f5da26d6bddbe738c613c905fb03757d
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-11 19:11:22 -0500

    still deployment bugs

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: BASE_URL,
+  credentials: 'include',
+});
 
 export const apiSlice = createApi({
   baseQuery,
