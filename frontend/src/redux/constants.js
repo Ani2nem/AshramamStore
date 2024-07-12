6a032d861351881919e5be60592cc40ba0e31c4d
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-11 22:33:52 -0500

    changed base url in constants frontend redux to the backend url on render

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "/"; 
+export const BASE_URL = "https://anirudh-e-store-backend.onrender.com"; 
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
