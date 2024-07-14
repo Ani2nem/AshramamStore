27051310c1f2b49fce5418e29abec2e6013af76d
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-14 07:25:51 -0500

    changed proxy in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy: {
-      "/api/": "http://localhost:5001"
+      "/api/": "https://anirudh-e-store.onrender.com"
     },
   },
 });
