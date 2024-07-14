121b8a578fbdfc0b4978f472997b5638388636ca
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-07-14 10:40:18 -0500

    vite config updated

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ import cors from 'cors';
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200 
 };
 
 app.use(cors(corsOptions));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     proxy: {
-      "/api/": "https://anirudh-e-store.onrender.com"
+      '/api': {
+        target: 'https://anirudh-e-store.onrender.com',
+        changeOrigin: true,
+        secure: false, // if the backend uses https and has a self-signed certificate
+      },
     },
   },
 });
